7114ab830173539451413df7f4be772453121a72
MLisman5113 <72448287+MLisman5113@users.noreply.github.com>
2023-05-18 13:38:28 -0400

    Delete ECON417 directory

--- ECON417/Math_Toolkit/creategrid1.jl
+++
@@ -1,9 +0,0 @@
-# Create an equally-spaced grid with npts points on the interval [xlow,xhigh].
-
-function creategrid(xlow,xhigh,npts)
-
-   xinc = (xhigh - xlow)/(npts-1)
-
-   return collect(xlow:xinc:xhigh)
-
-end
--- ECON417/PSETS/miscellaneous_pset_content/tempCodeRunnerFile.jl
+++
@@ -1,3 +0,0 @@
-function f(x)
-    return [(x[1]-2)^2+(x[2]-1)^2, (x[1]+x[2])^2]
-end
